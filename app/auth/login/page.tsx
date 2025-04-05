269c9605f9c8cf64f8dcb0613ea7f24b16234f08
Nylite-soda <109766268+Nylite-soda@users.noreply.github.com>
2025-04-05 10:47:12 +0100

    Delete app/auth/login directory

--- app/auth/login/page.tsx
+++
@@ -1,8 +0,0 @@
-            <div className="text-sm">
-              <Link
-                href="/auth/forgot-password?type=user"
-                className="font-medium text-primary hover:text-primary/80"
-              >
-                Forgot your password?
-              </Link>
-            </div> 
